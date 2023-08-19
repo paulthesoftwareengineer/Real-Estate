3072da3fbf19e56a0828c3ed1c2bc353b64a1ce8
Paul Turner <baldheader@live.com>
2023-08-18 21:53:41 -0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Add interactivity, animations, and other functionalities here
+$(document).ready(function() {
+    // Example: Hide carousel after 10 seconds
+    // setTimeout(function() {
+    //     $('#propertyCarousel').carousel('pause');
+    // }, 10000);
+});
